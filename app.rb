59c102afc731f7c3b0f569e3d4991524d2f63b8a
Tell-Becker <83348335+Tell-Becker@users.noreply.github.com>
2023-03-16 10:32:40 +0100

    All info is put in the explore tab

--- app.rb
+++ app.rb
@@ -49,11 +49,14 @@ post('/workout/create') do
     p "Vi fick in datan #{title}, #{muscletype}, #{exercise}, #{sets}, #{reps}"
     db = connect_to_db('db/Workout_app_database.db')
     muscle_id = db.execute("SELECT id FROM Muscles WHERE name = ?", muscletype).first()["id"]
+    exercise_id = db.execute("SELECT id FROM Exercises WHERE name = ?", exercise).first()["id"]
 
     db.execute("INSERT INTO Workouts (title, sets, reps, user_id) VALUES (?,?,?,?)",title, sets, reps, 1)
-    @workout_id = db.execute("SELECT id FROM Workouts ORDER BY id ASC").last["id"]
+    workout_id = db.execute("SELECT id FROM Workouts ORDER BY id ASC").last["id"]
     
     db.execute("INSERT INTO Workout_Muscle (workout_id, muscle_id) VALUES (?,?)",workout_id, muscle_id)
+
+    db.execute("INSERT INTO Workout_exercise (workout_id, exercise_id) VALUES (?,?)",workout_id, exercise_id)
     redirect('/workouts')
 end
 
@@ -87,10 +90,20 @@ helpers do
     def get_all_muscles_for_workout(workout_id)
         db = connect_to_db('db/Workout_app_database.db')
         return db.execute("
-            SELECT Muscles.[name]* 
+            SELECT Muscles.name 
             FROM Workout_Muscle 
             INNER JOIN Muscles, Workouts 
             ON Workout_Muscle.workout_id = Workouts.id AND Workout_Muscle.muscle_id = Muscles.id
+            WHERE Workouts.id = ?", workout_id)
+    end
+
+    def get_all_exercises_for_workout(workout_id)
+        db = connect_to_db('db/Workout_app_database.db')
+        return db.execute("
+            SELECT Exercises.name
+            FRom Workout_Exercise
+            Inner JOIN Exercises, Workouts
+            ON Workout_Exercise.workout_id = Workouts.id AND Workout_Exercise.exercise_id = Exercises.id
             WHERE Workouts.id = ?", workout_id)
     end
 end
