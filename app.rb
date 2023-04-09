2bc893a13620a665482aeca2431de9082406c7a8
Tell-Becker <83348335+Tell-Becker@users.noreply.github.com>
2023-04-09 13:47:54 +0200

    Working delete for workouts and users
    Admin can delete users, and users can delete their own exercises

--- app.rb
+++ app.rb
@@ -70,7 +70,7 @@ post('/workout/create') do
     p "Vi fick in datan #{title}, #{muscletype}, #{exercise}, #{sets}, #{reps}"
     # Funktion som gör att man enklare kan få in de olika övningarna i vardera tabell
     db = connect_to_db('db/Workout_app_database.db')
-    db.execute("INSERT INTO Workouts (title, sets, reps, user_id) VALUES (?,?,?,?)",title, sets, reps, session[:id])
+    # db.execute("INSERT INTO Workouts (title, sets, reps, user_id) VALUES (?,?,?,?)",title, sets, reps, session[:id])
     doNotAddTable = "nothing"
     add_values_to_tables(title, muscletype, exercise, sets, reps)
     add_values_to_tables(title, muscletype2, exercise2, sets2, reps2)
@@ -102,19 +102,99 @@ end
 get('/workouts/:id/edit') do 
     db = connect_to_db('db/Workout_app_database.db')
     id = params[:id].to_i
+    id2 = id + 1
+    id3 = id + 2 
+    id4 = id + 3 
+    id5 = id + 4 
+
     result = db.execute("SELECT * FROM Workouts WHERE id = ?", id).first
+    @result2 = db.execute("SELECT * FROM Workouts WHERE id = ?", id2).first
+    @result3 = db.execute("SELECT * FROM Workouts WHERE id = ?", id3).first
+    @result4 = db.execute("SELECT * FROM Workouts WHERE id = ?", id4).first
+    @result5 = db.execute("SELECT * FROM Workouts WHERE id = ?", id5).first
+
+
     slim(:"account/edit",locals:{workout:result})
 end
 
-post('workouts/:id/update') do 
+post('/workouts/:id/update') do 
     id = params[:id].to_i
     title = params[:title]
+    sets = params[:sets]
+    reps = params[:reps]
+    sets2 = params[:sets2]
+    reps2 = params[:reps2]
+    sets3 = params[:sets3]
+    reps3 = params[:reps3]
+    sets4 = params[:sets4]
+    reps4 = params[:reps4]
+    sets5 = params[:sets5]
+    reps5 = params[:reps5]
     db = connect_to_db('db/Workout_app_database.db')
-    db.execute("UPDATE Workouts SET content=? WHERE id = ?",title,id)
-    redirect(('/profile'))
+    if sets2 != nil
+        if db.execute("SELECT title FROM Workouts WHERE id = ?", id) == db.execute("SELECT title FROM Workouts WHERE id = ?", id + 1)
+            id2 = id + 1
+            db.execute("UPDATE Workouts SET title=? WHERE id = ?",title,id2)
+            db.execute("UPDATE Workouts SET sets=? WHERE id = ?",sets2, id2)
+            db.execute("UPDATE Workouts SET reps=? WHERE id = ?",reps2, id2)
+        end
+    end
+    if sets3 != nil
+        if db.execute("SELECT title FROM Workouts WHERE id = ?", id) == db.execute("SELECT title FROM Workouts WHERE id = ?", id + 2)
+            id3 = id + 2
+            db.execute("UPDATE Workouts SET title=? WHERE id = ?",title,id3)
+            db.execute("UPDATE Workouts SET sets=? WHERE id = ?",sets3, id3)
+            db.execute("UPDATE Workouts SET reps=? WHERE id = ?",reps3, id3)
+        end
+    end
+    if sets4 != nil
+        if db.execute("SELECT title FROM Workouts WHERE id = ?", id) == db.execute("SELECT title FROM Workouts WHERE id = ?", id + 3)
+            id4 = id + 3
+            db.execute("UPDATE Workouts SET title=? WHERE id = ?",title,id4)
+            db.execute("UPDATE Workouts SET sets=? WHERE id = ?",sets2, id4)
+            db.execute("UPDATE Workouts SET reps=? WHERE id = ?",reps2, id4)
+        end
+    end
+    if sets5 != nil
+        if db.execute("SELECT title FROM Workouts WHERE id = ?", id) == db.execute("SELECT title FROM Workouts WHERE id = ?", id + 4)
+            id5 = id + 4
+            db.execute("UPDATE Workouts SET title=? WHERE id = ?",title,id5)
+            db.execute("UPDATE Workouts SET sets=? WHERE id = ?",sets2, id5)
+            db.execute("UPDATE Workouts SET reps=? WHERE id = ?",reps2, id5)
+        end
+    end
+    # if db.execute("SELECT title FROM Workouts WHERE id = ?", id) == db.execute("SELECT title FROM Workouts WHERE id = ?", id + 4)
+    #     id5 = id + 4
+    #     db.execute("UPDATE Workouts SET title=? WHERE id = ?",title,id5)
+    # end
+    if sets != nil
+        db.execute("UPDATE Workouts SET title=? WHERE id = ?",title,id)
+        db.execute("UPDATE Workouts SET sets=? WHERE id = ?",sets, id)
+        db.execute("UPDATE Workouts SET reps=? WHERE id = ?",reps, id)
+    end
+    redirect('/profile')
 end
 
+post('/workouts/:id/delete') do 
+    db = connect_to_db('db/Workout_app_database.db')
+    id = params[:id].to_i
 
+    db.execute("DELETE FROM Workouts WHERE id = ?",id)
+    db.execute("DELETE FROM Workouts WHERE id = ?",id + 1)
+    db.execute("DELETE FROM Workouts WHERE id = ?",id + 2)
+    db.execute("DELETE FROM Workouts WHERE id = ?",id + 3)
+    db.execute("DELETE FROM Workouts WHERE id = ?",id + 4)
+
+
+    redirect('/profile')
+end
+
+post('/workouts/:id/delete_users') do 
+    db = connect_to_db('db/Workout_app_database.db')
+    id = params[:id].to_i
+    db.execute("DELETE FROM USERS WHERE id = ?",id)
+    redirect('/users')
+end
 # get('/workout') do 
 #     slim(:"account/index")
 # end
@@ -123,6 +203,12 @@ get('/profile') do
     db = connect_to_db('db/Workout_app_database.db')
     @result = db.execute("SELECT * FROM Workouts")
     slim(:"account/profile")
+end
+
+get('/users') do 
+    db = connect_to_db('db/Workout_app_database.db')
+    @result = db.execute("SELECT * FROM Users")
+    slim(:"account/users")
 end
 
 get('/register') do 
@@ -162,6 +248,7 @@ post('/finishlogin') do
 
     if pwdigest && BCrypt::Password.new(pwdigest) == password
         session[:id] = user_id
+        session[:username] = username
         redirect('/')
     else
         "Fel LÖSEN"
@@ -171,6 +258,7 @@ end
 
 get('/logout') do
     session[:id] = nil
+    session[:username] = nil
     redirect('/')
 end
 
@@ -209,7 +297,7 @@ helpers do
         db = connect_to_db('db/Workout_app_database.db')
         return db.execute("
             SELECT Exercises.name
-            FRom Workout_Exercise
+            FROM Workout_Exercise
             Inner JOIN Exercises, Workouts
             ON Workout_Exercise.workout_id = Workouts.id AND Workout_Exercise.exercise_id = Exercises.id
             WHERE Workouts.id = ?", workout_id)
