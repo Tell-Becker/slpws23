e3c5b588c801312cec623ae8bb08f96c503e14f8
Tell-Becker <83348335+Tell-Becker@users.noreply.github.com>
2023-05-01 03:29:43 +0200

    Yardoc
    Did yardoc on app.rb and model.rb
    dont know if i should do yardoc on the before statements due to them not appearing on the site.

--- app.rb
+++ app.rb
@@ -5,30 +5,66 @@ require 'sqlite3'
 require 'bcrypt'
 require_relative './model.rb'
 enable :sessions
+include Model 
 
+# Start that redirects to '/workouts', Makes it easier to understand the code
+#
 get('/') do
     redirect("/workouts")
 end
 
+# Display landing page that displays all created workouts, and correlating users
+#
+# @see Model#select_element_from_table
 get('/workouts') do 
 
-    @result = get_everything_from_table('Workouts')
-    @user_result = get_everything_from_table('Users')
+    @result = select_element_from_table('*','Workouts')
+    @user_result = select_element_from_table('*','Users')
 
     slim(:"account/index")
 
 end
 
+# Should before have yardoc code? It isn't displayed
+#
+before('/create') do 
+    if (session[:id] == nil)
+        halt 401, 'You cant enter this page unless you are logged in!'
+    end
+end
+
+# Displays a single button
+#
 get('/create') do
     slim(:"account/createButton")
 end
 
+# Before ? 
+before('/workout/new') do 
+    if (session[:id] == nil)
+        halt 401, 'You cant enter this page unless you are logged in!'
+    end
+end
+
+# Displays form containing title, exercises, muscles, sets and reps
+#
+# @see Model#select_element_from_table
 get('/workout/new') do
     @muscles = select_element_from_table('name','Muscles')
     @exercises = select_element_from_table('name','Exercises')
     slim(:"account/new")
 end
 
+# Creates a new workout and redirects to '/workouts
+#
+# @param [String] title, The title of the workout
+# @param [String] muscle, The muscle targeted by the workout
+# @param [String] exercise, The name of the exercise performed in the workout
+# @param [Integer] sets, The amount of sets performed in the exercise
+# @param [Integer] reps, The amount of reps to every set in the exercise
+#
+# @see Model#select_element_from_table
+# @see Model#add_values_to_tables
 post('/workout/create') do
     title = params[:title]
     muscletype = params[:muscletype1]
@@ -56,6 +92,14 @@ post('/workout/create') do
     sets5 = params[:sets5]
     reps5 = params[:reps5]
 
+    all_workouts_titles = select_element_from_table('title','Workouts')
+
+    all_workouts_titles.each do |workout_title|
+        if workout_title['title'] == title
+            halt 401, "Det finns redan en workout med denna titeln, testa att använda en annan titel"
+        end
+    end
+
     if sets != ""
         add_values_to_tables(title, muscletype, exercise, sets, reps)
     end
@@ -75,6 +119,10 @@ post('/workout/create') do
     redirect('/workouts')
 end
 
+# Displays an existing workout and gives opportunity to edit title, sets and reps 
+#
+# @see Model#select_element_from_table_where_id
+# @see Model#select_value_from_table_where_value
 get('/workouts/:id/edit') do 
     id = params[:id].to_i
     id2 = id + 1
@@ -118,6 +166,16 @@ get('/workouts/:id/edit') do
     slim(:"account/edit",locals:{workout:result})
 end
 
+# Updates an existing workout and redirects to '/profile'
+#
+# @param [Integer] id, The workout id
+# @param [String] title, The title of the workout
+# @param [Integer] sets, The amount of sets in the exercise
+# @param [Integer] reps, The amount of reps per set in the exercise
+#
+# @see Model#select_value_from_table_where_value
+# @see Model#select_element_from_table_where_id
+# @see Model#update_table_set_condition_where_id
 post('/workouts/:id/update') do 
     id = params[:id].to_i
     id2 = id + 1 
@@ -185,20 +243,19 @@ post('/workouts/:id/update') do
     redirect('/profile')
 end
 
+# Deletes an existing workout and redirects to '/profile'
+#
+# @param [Integer] id, The workout id
+#
+# @see Model#select_value_from_table_where_value
+# @see Model#select_element_from_table_where_id
+# @see Model#delete_from_table_where_id
 post('/workouts/:id/delete') do 
     id = params[:id].to_i
     id2 = id + 1 
     id3 = id + 2 
     id4 = id + 3 
     id5 = id + 4
-
-        # Behöver göra så att workoutsen tas bort från Workouts_Users !
-    # !
-    # !
-    # !
-    # !
-
-    #puts 'id:'
 
     what_user = select_value_from_table_where_value('user_id','Workouts','id',id)
 
@@ -231,18 +288,19 @@ post('/workouts/:id/delete') do
     redirect('/profile')
 end
 
+# Deletes workout displayed from '/workouts' if logged in user is ADMIN and redirects to '/workouts'
+#
+# @param [Integer] id, The workout id
+#
+# @see Model#select_value_from_table_where_value
+# @see Model#select_element_from_table_where_id
+# @see Model#delete_from_table_where_id
 post('/workouts/:id/delete_admin') do 
     id = params[:id].to_i
     id2 = id + 1 
     id3 = id + 2 
     id4 = id + 3 
     id5 = id + 4
-
-    # Behöver göra så att workoutsen tas bort från Workouts_Users !
-    # !
-    # !
-    # !
-    # !
 
     if session[:username] == "ADMIN"
         if select_element_from_table_where_id('title','Workouts','id',id) == select_element_from_table_where_id('title','Workouts','id',id2)
@@ -268,10 +326,15 @@ post('/workouts/:id/delete_admin') do
 
     end
 
-
     redirect('/workouts')
 end
 
+# Likes other users workouts and redirects to '/'
+#
+# @param [Integer] id, The workout id
+#
+# @see Model#select_value_from_table_where_value
+# @see Model#insert_into_table_item1_and_item2
 post('/workouts/:id/like_funktion') do
     id = params[:id].to_i
     what_users = select_value_from_table_where_value('Users_id','Workouts_Users','Workouts_id',id)
@@ -286,6 +349,10 @@ post('/workouts/:id/like_funktion') do
     redirect('/')
 end
 
+# Deletes an existing user if logged in user is admin ADMIN and redirects to '/users'
+#
+# @see Model#select_value_from_table_where_value
+# @see Model#delete_from_table_where_id
 post('/workouts/:id/delete_users') do 
     if session[:username] != "ADMIN"
         redirect('/')
@@ -294,22 +361,14 @@ post('/workouts/:id/delete_users') do
     if session[:username] == "ADMIN" 
         
         all_workouts_belonging_to_user = select_value_from_table_where_value('id','Workouts','user_id',id)
-        p "all_workouts_belonging_to_user"
-        p all_workouts_belonging_to_user
 
         all_workouts_belonging_to_user.each do |user_workouts|
-            p 'Hejsan hoppsan'
-            p user_workouts['id']
             delete_from_table_where_id('Workouts','id',user_workouts['id'])
         end
 
         all_liked_workouts_belonging_to_user = select_value_from_table_where_value('Workouts_id','Workouts_Users','Users_id',id)
-        p "all_liked_workouts_belonging_to_user"
-        p all_liked_workouts_belonging_to_user
 
         all_liked_workouts_belonging_to_user.each do |liked|
-            p 'hej'
-            p liked['Workouts_id']
             delete_from_table_where_id('Workouts_Users','Workouts_id',liked['Workouts_id'])
         end
 
@@ -317,36 +376,62 @@ post('/workouts/:id/delete_users') do
 
     end
 
-
     redirect('/users')
 end
 
+# Before?
+before('/profile') do 
+    if (session[:id] == nil)
+        halt 401, 'You cant enter this page unless you are logged in!'
+    end
+end
+
+# Displays workouts that user owns
+#
+# @see Model#select_element_from_table
 get('/profile') do
     @result = select_element_from_table('*','Workouts')
     slim(:"account/profile")
 end
 
+# Before ? 
+before('/users') do 
+    if (session[:username] != 'ADMIN')
+        halt 401, 'You cant enter this page unless you are ADMIN!'
+    end
+end
+
+# Displays all users
+#
 get('/users') do 
     @result = select_element_from_table('*','Users')
     slim(:"account/users")
 end
 
+# Displays a register form
+#
 get('/register') do 
     slim(:"register-login/register")
 end
 
+# Attempts registration
+#
+# @param [String] username, The username
+# @param [String] password, The password
+# @param [String] password confirmation, The password confirmation
+#
+# @see Model#select_element_from_table
+# @see Model#insert_into_table_item1_and_item2
 post('/finishregistration') do
     username = params[:username]
     password = params[:password] 
     password_confirm = params[:confirmpassword] 
 
     all_usernames = select_element_from_table('username','Users')
-    p "All_usernames:"
-    p all_usernames
 
     all_usernames.each do |users|
         if users['username'] == username
-            raise 'Detta användarnamn är redan taget'
+            halt 401, 'This username is already taken, make another one!'
             redirect('/register')
         end
     end
@@ -361,32 +446,60 @@ post('/finishregistration') do
 
 end
 
+#Displays a login form
+#
 get('/login') do 
     slim(:"register-login/login")
 end
 
+# Attempts login and updates the session and redirects to '/' if log in worked and 'login' if it didn't work
+#
+# @param [String] username, The username
+# @param [String] password, The password
+#
+# @see Model#select_value_from_table_where_value
 post('/finishlogin') do 
     username = params[:username]
     password = params[:password]
-    db = connect_to_db('db/Workout_app_database.db')
     user_login_info = select_value_from_table_where_value('*','Users','username',username)
-    # user_login_info = db.execute("SELECT * FROM Users WHERE username = ?", username)
 
-    if user_login_info
-        pwdigest = user_login_info[0][2]
-        user_id = user_login_info[0][0]
+    if session[:login_tries] == nil
+        session[:login_tries] = 0
     end
 
-    if pwdigest && BCrypt::Password.new(pwdigest) == password
-        session[:id] = user_id
-        session[:username] = username
-        redirect('/')
-    else
-        "Fel LÖSEN"
+    if session[:login_tries] >= 2
+
+        session[:last_login_time] = session[:last_login_time] || Time.now
+
+        if Time.now - session[:last_login_time] > 20 
+            session[:login_tries] = 0 
+            session[:last_login_time] = nil
+        else
+            return "You have tried to many times, you need to wait 20 seconds before trying to login again"
+        end
     end
-    
+
+    if user_login_info != []
+        if user_login_info
+            pwdigest = user_login_info[0][2]
+            user_id = user_login_info[0][0]
+        end
+
+        if pwdigest && BCrypt::Password.new(pwdigest) == password
+            session[:login_tries] = 0
+            session[:id] = user_id
+            session[:username] = username
+            redirect('/')
+        else
+            "Fel LÖSEN"
+        end
+    end
+    session[:login_tries] += 1 
+    redirect('/login')
 end
 
+# Logs user out and redirects to '/'
+#
 get('/logout') do
     session[:id] = nil
     session[:username] = nil
--- model.rb
+++ model.rb
@@ -1,87 +1,140 @@
-def connect_to_db(path)
-    db = SQLite3::Database.new('db/Workout_app_database.db')
-    db.results_as_hash = true
-    return db
-end
+module Model
 
-def get_everything_from_table(table)
-    db = connect_to_db('db/Workout_app_database.db')
+    # Connects to the SQLite3 database
+    #
+    # @param [String] path to database
+    #
+    # @return [Hash] way to interact with SQLite3 database
+    def connect_to_db(path)
+        db = SQLite3::Database.new('db/Workout_app_database.db')
+        db.results_as_hash = true
+        return db
+    end
 
-    return db.execute("SELECT * FROM #{table}")
+    # Attempts to select content from table
+    #
+    # @param [String] element The name of the element to select from the table
+    # @param [String] table The name of the table to select the element from
+    # 
+    # @return [Hash] containing data from element
+    def select_element_from_table(element,table)
+        db = connect_to_db('db/Workout_app_database.db')
 
-end
+        return db.execute("SELECT #{element} FROM #{table}")
+    end
 
-def select_element_from_table(element,table)
-    db = connect_to_db('db/Workout_app_database.db')
+    # Attempts to insert 5 contents into table
+    # 
+    # @param [String] title The title of the workout
+    # @param [String] muscletype The muscle targeted my the workout
+    # @param [String] exercise The name of the exercise performed in the workout
+    # @param [Integer] sets The amount of sets performed in the exercise
+    # @param [Integer] reps The akount of reps for every set in the exerise
+    def add_values_to_tables(title, muscletype, exercise, sets, reps)
+        db = connect_to_db('db/Workout_app_database.db')
+        
+        db.execute("INSERT INTO Workouts (title, sets, reps, user_id) VALUES (?,?,?,?)",title, sets, reps, session[:id])
+        workout_id = db.execute("SELECT id FROM Workouts WHERE title = ?",title)
+        workout_id = workout_id.last["id"]
+        db.execute("INSERT INTO Workouts_Users (Workouts_id, Users_id) VALUES (?,?)", workout_id, session[:id])
+        muscle_id = db.execute("SELECT id FROM Muscles WHERE name = ?", muscletype).first()["id"]
+        exercise_id = db.execute("SELECT id FROM Exercises WHERE name = ?", exercise).first()["id"]
 
-    return db.execute("SELECT #{element} FROM #{table}")
-end
+        workout_id = db.execute("SELECT id FROM Workouts ORDER BY id ASC").last()["id"]
+        
+        db.execute("INSERT INTO Workout_Muscle (workout_id, muscle_id) VALUES (?,?)",workout_id, muscle_id)
 
-def add_values_to_tables(title, muscletype, exercise, sets, reps)
-    db = connect_to_db('db/Workout_app_database.db')
+        db.execute("INSERT INTO Workout_exercise (workout_id, exercise_id) VALUES (?,?)",workout_id, exercise_id)
     
-    db.execute("INSERT INTO Workouts (title, sets, reps, user_id) VALUES (?,?,?,?)",title, sets, reps, session[:id])
-    workout_id = db.execute("SELECT id FROM Workouts WHERE title = ?",title)
-    # p "workout_id #{workout_id}"
-    workout_id = workout_id.last["id"]
-    # p "workout_id #{workout_id}"
-    db.execute("INSERT INTO Workouts_Users (Workouts_id, Users_id) VALUES (?,?)", workout_id, session[:id])
-    muscle_id = db.execute("SELECT id FROM Muscles WHERE name = ?", muscletype).first()["id"]
-    exercise_id = db.execute("SELECT id FROM Exercises WHERE name = ?", exercise).first()["id"]
+    end
 
-    workout_id = db.execute("SELECT id FROM Workouts ORDER BY id ASC").last()["id"]
-    
-    db.execute("INSERT INTO Workout_Muscle (workout_id, muscle_id) VALUES (?,?)",workout_id, muscle_id)
+    # Attempts to select the first content from table from a certain id
+    #
+    # @param [String] element The name of the column to select an element from 
+    # @param [String] table The name of the table to select the element from
+    # @param [String] what_id The type of id to select the element from
+    # @param [Integer] id The id of the row to be selected
+    #
+    # @return [Hash] containing the first data of from the matching id
+    def select_element_from_table_where_id(element,table,what_id,id)
+        db = connect_to_db('db/Workout_app_database.db')
 
-    db.execute("INSERT INTO Workout_exercise (workout_id, exercise_id) VALUES (?,?)",workout_id, exercise_id)
-   
-end
+        return db.execute("SELECT #{element} FROM #{table} WHERE #{what_id} = ?",id).first
+    end
 
-def select_element_from_table_where_id(element,table,what_id,id)
-    db = connect_to_db('db/Workout_app_database.db')
+    # Attempts to update a row on a table 
+    #
+    # @param [String] what_table The name of the table to update
+    # @param [String] what_condition The name of the column to update
+    # @param [String] condition The new value to give the column
+    # @param [Integer] id The id of the row to update
+    def update_table_set_condition_where_id(what_table,what_condition,condition,id)
+        db = connect_to_db('db/Workout_app_database.db')
 
-    return db.execute("SELECT #{element} FROM #{table} WHERE #{what_id} = ?",id).first
-end
+        db.execute("UPDATE #{what_table} SET #{what_condition} = ? WHERE id = ?",condition,id)
+    end
 
-def update_table_set_condition_where_id(what_table,what_condition,condition,id)
-    db = connect_to_db('db/Workout_app_database.db')
+    # Attempt to delete a row on a table
+    #
+    # @param [String] table The name of the table to delete from
+    # @param [String] what_id The type of id to delete from
+    # @param [Integer] id The id of the row to be deleted
+    def delete_from_table_where_id(table,what_id,id)
+        db = connect_to_db('db/Workout_app_database.db')
 
-    return db.execute("UPDATE #{what_table} SET #{what_condition} = ? WHERE id = ?",condition,id)
-end
+        db.execute("DELETE FROM #{table} WHERE #{what_id} = ?",id)
+    end
 
-def delete_from_table_where_id(table,what_id,id)
-    db = connect_to_db('db/Workout_app_database.db')
+    # Attempts to insert two id:s into a table
+    #
+    # @param [String] table The name of table to insert into
+    # @param [String] what_id1 The type of id to insert into
+    # @param [String] what_id2 The other type of id to insert into
+    # @param [Integer] value1 The value to be inserted into the column
+    # @param [Integer] value The other value to be inserted into column
+    def insert_into_table_item1_and_item2(table,what_id1,what_id2,value1,value2)
+        db = connect_to_db('db/Workout_app_database.db')
 
-    return db.execute("DELETE FROM #{table} WHERE #{what_id} = ?",id)
-end
+        db.execute("INSERT INTO #{table} (#{what_id1},#{what_id2}) VALUES (?,?)",value1,value2)
+    end
 
-def insert_into_table_item1_and_item2(table,what_id1,what_id2,value1,value2)
-    db = connect_to_db('db/Workout_app_database.db')
+    # Attempts to select content from table from a certain id
+    #
+    # @param [String] element The name of the column to select an element from 
+    # @param [String] table The name of the table to select the element from
+    # @param [String] what_id The type of id to select the element from
+    # @param [Integer] id The id of the row to be selected
+    #
+    # @return [Hash] containing the data from the matching id
+    def select_value_from_table_where_value(element,table,what_id,id)
+        db = connect_to_db('db/Workout_app_database.db')
 
-    return db.execute("INSERT INTO #{table} (#{what_id1},#{what_id2}) VALUES (?,?)",value1,value2)
-end
+        return db.execute("SELECT #{element} FROM #{table} WHERE #{what_id} = ?",id)
+    end
 
-def select_value_from_table_where_value(id1,table,what_id,id)
-    db = connect_to_db('db/Workout_app_database.db')
+    # Attempts to get username from workout_id
+    #
+    # @param [Integer] workout_id The value of the workout id
+    #
+    # @return [Hash] contaning the usernames from the matching id
+    def get_username_from_workout_id(workout_id)
+        db = connect_to_db('db/Workout_app_database.db')
 
-    return db.execute("SELECT #{id1} FROM #{table} WHERE #{what_id} = ?",id)
-end
+        list_of_users = db.execute("SELECT Users.username
+                        FROM Workouts_Users
+                        INNER JOIN Workouts, Users
+                        ON Workouts_Users.Workouts_id = Workouts.id AND Workouts_Users.Users_id = Users.id 
+                        WHERE Workouts.id = ?", workout_id)
 
-def get_user_id_from_workout_id(workout_id)
-    db = connect_to_db('db/Workout_app_database.db')
+        return list_of_users
 
-    list_of_users = db.execute("SELECT Users.username
-                    FROM Workouts_Users
-                    INNER JOIN Workouts, Users
-                    ON Workouts_Users.Workouts_id = Workouts.id AND Workouts_Users.Users_id = Users.id 
-                    WHERE Workouts.id = ?", workout_id)
+    end
 
-    # puts "List_of_users:"
-    return list_of_users
-
-end
-
-helpers do
+    # Attempts to get all muscles from workout_id
+    #
+    # @param [Integer] workout_id The value of the workout id
+    #
+    # @return [Hash] contaning the muscles from the matching id
     def get_all_muscles_for_workout(workout_id)
         db = connect_to_db('db/Workout_app_database.db')
         return db.execute("
@@ -92,6 +145,11 @@ helpers do
             WHERE Workouts.id = ?", workout_id)
     end
 
+    # Attempts to get all exercises from workout_id
+    #
+    # @param [Integer] workout_id The value of the workout id
+    #
+    # @return [Hash] contaning the exercises from the matching id
     def get_all_exercises_for_workout(workout_id)
         db = connect_to_db('db/Workout_app_database.db')
         return db.execute("
@@ -101,4 +159,5 @@ helpers do
             ON Workout_Exercise.workout_id = Workouts.id AND Workout_Exercise.exercise_id = Exercises.id
             WHERE Workouts.id = ?", workout_id)
     end
+
 end
